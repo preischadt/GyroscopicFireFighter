ea5c8aa0e6a6d5ec1200ac0f815d12a8eef20fcc
preischadt <lucas_p@outlook.com>
2020-02-02 11:16:13 -0300

    Things

---
+++ Assets/Extinguisher.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Extinguisher : MonoBehaviour
+{
+    public ParticleSystem particle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StopFiring();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO touch
+        if (Input.GetMouseButton(0) || Input.touchCount > 0)
+        {
+            StartFiring();
+        }
+        else
+        {
+            StopFiring();
+        }
+    }
+
+    void StartFiring()
+    {
+        if (!particle.isEmitting)
+        {
+            particle.Play();
+        }
+    }
+
+    void StopFiring()
+    {
+        particle.Stop();
+    }
+}
---
+++ Assets/MouseRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotation : MonoBehaviour
+{
+     public float sensitivity = 10f;
+     private Vector2 currentRotation;
+
+    void Start(){
+        Cursor.visible = false;
+    }
+
+     void Update()
+     {
+         currentRotation.x += Input.GetAxis("Mouse X") * sensitivity;
+         currentRotation.y -= Input.GetAxis("Mouse Y") * sensitivity;
+         currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
+         Camera.main.transform.rotation = Quaternion.Euler(currentRotation.y,currentRotation.x,0);
+         if (Input.GetMouseButtonDown(0))
+             Cursor.lockState = CursorLockMode.Locked;
+     }
+}
