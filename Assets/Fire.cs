ce198d68ac89d2460805cbcf855120feeeb7f882
preischadt <lucas_p@outlook.com>
2020-02-02 13:03:48 -0300

    YES

---
+++ Assets/Fire.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    float maxLife = 0.5f;
+    float currentLife;
+    public ParticleSystem particle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        currentLife -= Time.deltaTime;
+        Debug.Log($"Collision: {currentLife}");
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (currentLife <= i * maxLife / transform.childCount)
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void Spawn()
+    {
+        currentLife = maxLife;
+    }
+}
