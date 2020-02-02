886f0d2fa74204713b2ed1d4c6bad3fc1faaa4b0
Lucas Pinheiro <lucas.pinheiro@tapps.com.br>
2020-02-01 22:22:36 -0300

    Camera movement

---
+++ Assets/DeviceRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceRotation : MonoBehaviour
+{
+    private GameObject camParent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        camParent = new GameObject("CamParent");
+        camParent.transform.position = transform.position;
+        transform.parent = camParent.transform;
+        Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camParent.transform.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);
+        transform.Rotate(-Input.gyro.rotationRateUnbiased.x, 0, 0);
+    }
+}
